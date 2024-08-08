32d8afa56c4d93bc8d4e541e2c0970c5c60eb0d0
Fennec-meow <krisbojchenko@gail.com>
2024-08-08 15:30:46 +0300

    Initial Commit

---
+++ two squares/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task1
+//
+//  Created by Kira on 08.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
